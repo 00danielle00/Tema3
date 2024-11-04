f4b6e846a718c4ffd6df06d099fc3fcf5fa4c896
A703\1dam <danielleludovicoo@gmail.com>
2024-11-04 21:14:41 +0100

    subo ejercicio1 de vectores

--- Tema-3/src/main/java/org/example/Main.java
+++ Tema-3/src/main/java/org/example/Main.java
@@ -3,5 +3,11 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void main(String[] args) {
+
+        Arrays ejercicio1 = new Arrays();
+        ejercicio1.ejercicio1();
+
+    }
 
 }
