3efa4263ac881aad05f90b35345e9d78babc6a08
DESKTOP-AI1DKKS\Dani <danielleludovicoo@gmail.com>
2024-11-08 12:25:16 +0100

    Hago commit

--- Tema-3/src/main/java/org/example/Teoria_vectores.java
+++ Tema-3/src/main/java/org/example/Teoria_vectores.java
@@ -6,24 +6,62 @@ public class Teoria_vectores {
     public void teoria(){
 //guardar numero en el vector
         Scanner teclado = new Scanner(System.in);
-        int numero;
-        int sumaprecios = 0;
-        int precios[]= new int[3];
+//        int numero;
+//        int sumaprecios = 0;
+//        int precios[]= new int[3];
+//
+//
+//        for (int i=0; i< precios.length;i++){
+//            System.out.println("dame el precio: ");
+//            precios[i]= teclado.nextInt();
+//        }
+//        for (int i=0; i< precios.length;i++){
+//            System.out.println(precios[i]);
+//        }
+//
+//    //sumar vectores
+//        for (int i=0; i< precios.length;i++){
+//            sumaprecios=sumaprecios+precios[i];
+//        }
+//        System.out.println("La suma es "+sumaprecios);
+// vector será del tamaño que el usuario nos indique
 
+        System.out.println("Cuántos números quieres guardar?");
+        int longitud= teclado.nextInt();
 
-        for (int i=0; i< precios.length;i++){
-            System.out.println("dame el precio: ");
-            precios[i]= teclado.nextInt();
+        int array []= new int[longitud];
+        int aux;
+//for para llenar el array
+        for (int i=0; i<longitud;i++){
+            System.out.println("Ingrese el numero "+ (i+1)+":");
+            aux= teclado.nextInt();
+//Se guarda el numero introducido anteriormente en el array
+            array[i]=aux;
         }
-        for (int i=0; i< precios.length;i++){
-            System.out.println(precios[i]);
+        //for para imprimir
+        System.out.println("Array desordenado");
+        for (int i=0; i<longitud;i++){
+            System.out.println(array[i]);
         }
 
-    //sumar vectores
-        for (int i=0; i< precios.length;i++){
-            sumaprecios=sumaprecios+precios[i];
+        //ordenar el array en caso de que el usuario haya rellenado sin ordenar
+        int guardado;
+        //que lo haga una vez menos, lo ordena por primera vez
+        for (int i=0;i<(longitud-1);i++){
+        //lo ordena por 2 vez, lo reinicia
+            for (int j=0;j<(longitud-1);j++){
+                if (array[j]>array[j+1]){
+                    guardado=array[j];//guarda el primer valor
+                    array[j]=array[j+1];
+                    array[j+1]=guardado;
+                }
+            }
         }
-        System.out.println("La suma es "+sumaprecios);
+        System.out.println("Array ordenado");
+        for (int i=0; i<longitud;i++){
+            System.out.println(array[i]);
+        }
+
 
 
 
