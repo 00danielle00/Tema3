618d7be5a132cd578b3cc91de1e492373307dcd2
DESKTOP-AI1DKKS\Dani <danielleludovicoo@gmail.com>
2024-11-09 12:26:09 +0100

    commit

--- Tema-3/src/main/java/org/example/Samurais.java
+++ Tema-3/src/main/java/org/example/Samurais.java
@@ -5,28 +5,58 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Samurais {
-    public void samurai(){
+    public void samurai() {
 
         Scanner teclado = new Scanner(System.in);
         Random aleatorio = new Random();
 
-        int equipo1[]=new int[7];
-        int equipo2[]=new int[7];
+        int equipo1[] = new int[7];
+        int equipo2[] = new int[7];
 
-        int suma=0;
+        int suma = 0;
+        int suma2 = 0;
 
+        boolean comprobador = true;
         System.out.println("Equipo 1:");
-        for (int i=0;i<7;i++){
-            System.out.print("Introduce la potencia de los samurais:");//pregunto la potencia de los samurais
-            String potenciasamurai=teclado.next();//introduce los valores de la potencia
-            String entrada []=potenciasamurai.split(" ");
+        System.out.print("Introduce la potencia de los samurais:");//pregunto la potencia de los samurais
 
-            if (entrada.length == 7){//si las potencias son igual a 7
-                equipo1[i]=Integer.parseInt(entrada[i]); //pasamos a entero el numero
-                suma+=equipo1[i];
+
+        while (comprobador) {
+            String potenciasamurai = teclado.next();//introduce los valores de la potencia
+            String entrada[] = potenciasamurai.split(" ");//utilizo el delimitador utilizando espacios
+
+            if (entrada.length == 7) {//si las el índice son igual a 7
+
+                for (int i = 0; i < 7; i++) {
+                    equipo1[i] = Integer.parseInt(entrada[i]); //pasamos a entero el numero
+                    suma += equipo1[i];//sumamos todos los índices dentro del array
+
+                    if (suma != 30) {
+                        System.out.println("Error.La potencia total no suma 30");
+                        comprobador=false;
+                        potenciasamurai = teclado.next();
+                    } else {
+                        System.out.println("Equipo completado");
+                        return;
+                    }
+                }
             }
-
-
         }
+//        System.out.println("Equipo 2:");
+//        System.out.print("Introduce la potencia de los samurais:");//pregunto la potencia de los samurais
+//        String potenciaequipo2 = teclado.next();//introduce los valores de la potencia
+//        String entrada2[] = potenciaequipo2.split(" ");//utilizo el delimitador utilizando espacios
+//
+//        for (int i = 0; i < 7; i++) {
+//            equipo2[i] = Integer.parseInt(entrada2[i]); //pasamos a entero el numero
+//            suma2 += equipo2[i];//sumamos todos los índices dentro del array
+//
+//            if (suma != 30) {
+//                System.out.println("Error.La potencia total no suma 30");
+//                potenciaequipo2 = teclado.next();
+//            } else {
+//                System.out.println("Equipo completado");
+//            }
+//        }
     }
 }
